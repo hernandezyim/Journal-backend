da800b6d164dc952f5ac04ff1d5d2faa48ba87b9
Yim Hernandez <yimhernadez1@hotmail.com>
2022-08-01 19:57:49 -0400

    Add new configs firebase

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,8 +1,31 @@
-import { initializeApp, applicationDefault } from "firebase-admin/app";
+import admin from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
+import {
+  FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  FIREBASE_AUTH_URI,
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_CLIENT_ID,
+  FIREBASE_CLIENT_X509_CERT_URL,
+  FIREBASE_PRIVATE_KEY,
+  FIREBASE_PRIVATE_KEY_ID,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_TOKEN_URI,
+  FIREBASE_TYPE,
+} from "./keys.js";
 
-initializeApp({
-  credential: applicationDefault(),
+admin.initializeApp({
+  credential: admin.credential.cert({
+    type: FIREBASE_TYPE,
+    project_id: FIREBASE_PROJECT_ID,
+    private_key_id: FIREBASE_PRIVATE_KEY_ID,
+    private_key: FIREBASE_PRIVATE_KEY,
+    client_email: FIREBASE_CLIENT_EMAIL,
+    client_id: FIREBASE_CLIENT_ID,
+    auth_uri: FIREBASE_AUTH_URI,
+    token_uri: FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: FIREBASE_CLIENT_X509_CERT_URL,
+  }),
 });
 
 console.log("Database online");
